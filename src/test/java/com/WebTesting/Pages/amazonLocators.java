a75b9ab1e9bccc7a349c33177b5393b251edbbf4
adrothemaster <81396222+adrothemaster@users.noreply.github.com>
2021-10-05 20:57:56 -0500

    new push 2

--- src/test/java/com/WebTesting/Pages/amazonLocators.java
+++ src/test/java/com/WebTesting/Pages/amazonLocators.java
@@ -1,4 +1,27 @@
 package com.WebTesting.Pages;
 
+import com.WebTesting.Utils.Driver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class amazonLocators {
+
+    public amazonLocators(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(id = "twotabsearchtextbox")
+    public WebElement searchBar;
+
+    @FindBy(id = "nav-search-bar-form")
+    public WebElement searchBarSearchButton;
+
+    @FindBy(linkText = "OXO")
+    public WebElement OXOClick;
+
+
+
+
 }
--- src/test/java/com/WebTesting/Tests/amazonTest.java
+++ src/test/java/com/WebTesting/Tests/amazonTest.java
@@ -1,7 +1,32 @@
 package com.WebTesting.Tests;
 
+import com.WebTesting.Utils.ConfigurationReader;
+import com.WebTesting.Utils.Driver;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
 public class amazonTest {
 
+    /**
+     * User on the amazon search page
+     * user navigates to search field enters wooden spoon
+     * user selects OXO check box and selects first project
+     * user verifies selection the adds to cart
+     * user checks out
+     */
 
 
+
+
+
+    @BeforeMethod
+    public static void urlStart(){
+        Driver.getDriver().get(ConfigurationReader.getProperties("amazonUrl"));}
+
+    @Test
+    public void amazonTestCase(){
+
+
+
+}
 }
